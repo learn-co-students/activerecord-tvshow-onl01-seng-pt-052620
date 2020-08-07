caacfe005071c5a571a6b62140a392840f37792f
pete3249 <katelynrpeterson16@gmail.com>
2020-08-07 08:12:31 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,33 @@
 class Show < ActiveRecord::Base
 
     def self.highest_rating
-       Show.maximum(:rating) 
+       maximum("rating") 
     end 
 
     def self.most_popular_show
-        
+        order("rating DESC").first
+        #where("rating = ?", self.highest_rating)[0]
+    end 
+
+    def self.lowest_rating
+        minimum("rating")
+    end 
+
+    def self.least_popular_show
+        order("rating").first
+        # where("rating = ?", self.lowest_rating)[0]
+    end 
+
+    def self.ratings_sum
+        sum("rating")
+    end 
+
+    def self.popular_shows
+        where("rating > ?", 5)
+    end 
+
+    def self.shows_by_alphabetical_order
+        order("name")
     end 
 
 end 
